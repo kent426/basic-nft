5ee2607b65f3bbe6fd7b269d9198c36b00e8ac73
Kent Huang <kenthuang426@gmail.com>
2021-10-25 10:37:52 +0800

    installed hardhat plugin and setup basic config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,31 @@
 import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
+
+//
+// Select the network you want to deploy to here:
+//
+const defaultNetwork = "localhost";
 
 const config: HardhatUserConfig = {
   solidity: "0.7.3",
+  defaultNetwork,
+
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+  },
 };
 
 /**
