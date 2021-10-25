1b034877138b4b6dfa7dcd1eec263c1491e89852
Kent Huang <kenthuang426@gmail.com>
2021-10-25 18:23:56 +0800

    modified BasicNFT

--- contracts/BasicNFT.sol
+++ contracts/BasicNFT.sol
@@ -5,27 +5,31 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-// import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 
+// import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 
 contract BasicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    uint256 public totalSupply = 1000;
 
-    constructor() ERC721("BasicNFT", "BNFT") {
-    }
+    event MintedBNFT(address to, string tokenuri);
 
-        function mintBNFT(address recipient, string memory tokenuri)
+    constructor() ERC721("BasicNFT", "BNFT") {}
+
+    function mintBNFT(address recipient, string memory tokenuri)
         public
         returns (uint256)
     {
-        require(balanceOf(msg.sender) != 0, "Can not mint more than one BNFT");
+        require(balanceOf(msg.sender) == 0, "Can not mint more than one BNFT");
+
         _tokenIds.increment();
+        uint256 currentItemId = _tokenIds.current();
+        require(currentItemId < 1000);
 
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenuri);
-
-        return newItemId;
+        _safeMint(recipient, currentItemId);
+        _setTokenURI(currentItemId, tokenuri);
+        emit MintedBNFT(recipient, tokenuri);
+        return currentItemId;
     }
 }
