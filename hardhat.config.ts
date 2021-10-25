775320869b3c1520107112c7df6b9b2fdf8c6306
Kent Huang <kenthuang426@gmail.com>
2021-10-25 10:22:04 +0800

    added ts

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.7.3",
+};
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+export default config;
